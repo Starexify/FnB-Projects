8760a5ac1fceefe78f839feaa0d5046ba5e8381d
ImVeryBad <nightbear843@yahoo.com>
2025-08-20 12:36:28 +0200

    Testin 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 }
